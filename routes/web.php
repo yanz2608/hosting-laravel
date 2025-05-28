5589507747d4913c02c0261cb812d0528aaee7f7
yanz2608 <yanzarfianenrista@gmail.com>
2025-05-28 11:23:12 +0700

    tambah dikit

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+use App\Http\Controllers\TaskController;
+
+Route::get('/', [TaskController::class, 'index']);
+Route::post('/tasks', [TaskController::class, 'store']);
+Route::put('/tasks/{task}', [TaskController::class, 'update']);
+Route::delete('/tasks/{task}', [TaskController::class, 'destroy']);
